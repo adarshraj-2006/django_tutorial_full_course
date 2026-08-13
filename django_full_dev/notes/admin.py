e3ef0d743957177ec42039b146b9641e564956e1
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-08-14 05:06:28 +0530

    adding models of notes and performing migrations and also django shell

---
+++ django_full_dev/notes/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from . import models
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title','text','date_created')
+admin.site.register(models.Note,NotesAdmin)
---
+++ django_full_dev/notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title =models.CharField(max_length=200)
+    text=models.TextField()
+    date_created=models.DateTimeField(auto_now_add=True)
+    no_of_likes=models.PositiveIntegerField(default=0)
