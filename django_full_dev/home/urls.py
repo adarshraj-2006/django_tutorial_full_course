c38b832fd275eeb97725652393e9b03eab110e85
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-08-14 17:03:34 +0530

    use dynamic template,404 error,try-except block,class use instead of function

--- django_full_dev/home/urls.py
+++ django_full_dev/home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.home),
-    path('authorize',views.authorize)
+    path('home',views.HomeView.as_view()),
+    path('authorize',views.AuthorizeView.as_view),
 ]
--- django_full_dev/notes/views.py
+++ django_full_dev/notes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Note
+from django.http import Http404
 # Create your views here.
+def list(request):
+    
+    all_notes=Note.objects.all()
+    
+
+    return render(request,'notes/notes_list.html',{'notes':all_notes})
+
+def detail(request,pk):
+    try:
+        note =Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+            raise Http404("Note does not exist")
+    return render(request,'notes/notes_detail.html',{'notes':note})
+
