06fd3dacb1f969ac9a4626f3d7750f9e31d6ce82
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-08-21 15:34:02 +0530

    final commit

--- django_full_dev/notes/urls.py
+++ django_full_dev/notes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
     path('notes/', views.NoteList.as_view(), name='notes.list'),
     path('notes/<int:pk>/', views.NoteDetail.as_view(), name='notes.detail'),
     path('notes/<int:pk>/edit/', views.NotesUpdateView.as_view(), name='notes.update'),
     path('notes/<int:pk>/delete/', views.NotesDeleteView.as_view(), name='notes.delete'),
     path('notes/new/', views.NotesCreateView.as_view(), name='notes.new'),
+    path('login',views.LoginInterfaceView.as_view(),name='login'),
+    path('logout',views.LogoutInterfaceView.as_view(),name='logout'),
 ]
--- django_full_dev/notes/views.py
+++ django_full_dev/notes/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
 from .models import Note
-from django.http import Http404
-from django.views.generic import CreateView,ListView,DetailView,UpdateView
+from django.http import Http404,HttpResponseRedirect
+from django.views.generic import CreateView,ListView,DetailView,UpdateView,TemplateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView,LogoutView
 # Create your views here.
 
 from .forms import NotesForm
 
+class HomeView(TemplateView):
+     template_name='home/welcome.html'
+class LogoutInterfaceView(LogoutView):
+    next_page='notes/logout.html'
+class LoginInterfaceView(LoginView):
+     template_name='notes/login.html'
+
+
 
 class NotesDeleteView(DeleteView):
+
      model=Note
      success_url='/notes'
-     form_class=NotesForm
-    
+     context_object_name = 'note'
+     template_name='notes/notes_delete.html'
 
 class NotesUpdateView(UpdateView):
      model=Note
@@ -20,16 +31,26 @@ class NotesUpdateView(UpdateView):
      success_url='/notes'     
      template_name='notes/notes_form.html'
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
      model=Note
      form_class=NotesForm
      success_url='/notes'
      template_name='notes/notes_form.html'
-class NoteList(ListView):
+
+     def form_valid(self,form):
+          self.object=form.save(commit=False)
+          self.object.user=self.request.user
+          self.object.save()
+          return HttpResponseRedirect(self.get_success_url())
+class NoteList(LoginRequiredMixin, ListView):
      model = Note
      context_object_name = 'notes'      
      template_name ='notes/notes_list.html'
+     login_url='/admin/login/'
 
+     def get_queryset(self):
+          return self.request.user.notes.all()
+     
 class NoteDetail(DetailView):
      model = Note
      context_object_name = 'note'
