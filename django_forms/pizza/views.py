d094b2b6f20b53645bcb6633db1fa195350cb606
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-08-24 01:35:52 +0530

    working with widget & advance widget

--- django_forms/pizza/views.py
+++ django_forms/pizza/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
+from .forms import PizzaForm
+
 # from django.http import HttpResponse
 
 def home(request):
     return render(request,'pizza/home.html')
 def order(request):
-    if request.method == "POST":
-        topping1 = request.POST.get("topping1")
-        topping2 = request.POST.get("topping2")
-        size = request.POST.get("size")
+    if (request.method == 'POST'):
+        filled_form =PizzaForm(request.POST, request.FILES)
+        if filled_form.is_valid():
+            note= 'Thanks for ordering ! you %s %s amd %s pizza on way' %(filled_form.cleaned_data['size'],
+            filled_form.cleaned_data['topping1'],
+            filled_form.cleaned_data['topping2'])
 
-        print(topping1, topping2, size)
+            return render(request, 'pizza/order.html', {
+                'note': note,
+                'pizzaform': filled_form
+            })
+    
+    else:
+        form =PizzaForm()
 
-    return render(request, 'pizza/order.html')
+        return render(request, 'pizza/order.html',{'pizzaform':form})
